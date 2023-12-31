4c4d4d15117e2fa43b3d48d83116c7f5b31cb767
Chathurika Gajanayaka <chathurikagajanayaka@chathurikas-air.home>
2023-12-31 03:33:22 +0100

    Restructure application: on mentors comments

--- The_Dine_restaurant/settings.py
+++ The_Dine_restaurant/settings.py
@@ -31,7 +31,6 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'customer.apps.CustomerConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- The_Dine_restaurant/urls.py
+++ The_Dine_restaurant/urls.py
@@ -18,6 +18,5 @@ from django.contrib import admin
 from django.urls import include,     path
 
 urlpatterns = [
-    path("customer/", include("customer.urls")),
     path('admin/', admin.site.urls),
 ]
