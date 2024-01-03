8c3c46bee0eae4b55143c2df9da7a6bbf9bca4d9
Chathurika Gajanayaka <chathurikagajanayaka@chathurikas-air.home>
2024-01-03 01:22:42 +0100

    generated models

--- reservation/models.py
+++ reservation/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
 # Create your models here.
+
+
+class TimeSlot(models.Model):
+    time_slot = models.CharField(max_length=15)
+
+    def __str__(self) -> str:
+        return self.time_slot
+
+
+class Customer(models.Model):
+    first_name = models.CharField(max_length=25)
+    last_name = models.CharField(max_length=25)
+    email = models.CharField(max_length=100)
+    phone = models.CharField(max_length=18)
+
+    def __str__(self) -> str:
+        return self.first_name + " " + self.last_names
+
+
+class Reservation(models.Model):
+    time_slot = models.ForeignKey(TimeSlot, on_delete=models.CASCADE)
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    reservation_date = models.DateField("Reservation date")
+    number_of_guests_int = models.IntegerField(default=2)
+
+    def __str__(self) -> str:
+        return self.reservation_date
