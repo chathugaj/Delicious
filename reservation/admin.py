b5f1a0c2ad873bacd70d35d93a1d432c3edcb46c
Chathurika Gajanayaka <chathurikagajanayaka@chathurikas-air.home>
2024-01-03 01:32:59 +0100

    Register models in admin

--- reservation/admin.py
+++ reservation/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
+from .models import TimeSlot, Customer, Reservation
+
 # Register your models here.
+admin.site.register(TimeSlot)
+admin.site.register(Customer)
+admin.site.register(Reservation)
