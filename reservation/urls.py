89ceeb42e8d01c0ff9569902e96d7ea855d77661
Chathurika Gajanayaka <chathurikagajanayaka@chathurikas-air.home>
2024-01-04 04:09:43 +0100

    View book table form properly with data

--- reservation/urls.py
+++ reservation/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index"),
+    path("", views.table_details, name="index"),
     path("table/", views.table_details, name="table"),
     path("customer/", views.customer_details, name="customer")
 ]
