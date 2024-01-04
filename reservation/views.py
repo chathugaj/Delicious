3fbdca6771328eb92db12efd14558fb456eb9c64
Chathurika Gajanayaka <chathurikagajanayaka@chathurikas-air.home>
2024-01-04 03:59:11 +0100

    book table screen added

--- reservation/views.py
+++ reservation/views.py
@@ -1,8 +1,26 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import TimeSlot
+
 # Create your views here.
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the reservations")
+    return HttpResponse("RESERVATION index")
+
+
+def reservations(request):
+    return HttpResponse("reservations")
+
+
+def table_details(request):
+    time_slot_list = TimeSlot.objects.all()
+    context = {
+        "time_slot_list": time_slot_list
+    }
+    return render(request, "reservation/table_details.html", context)
+
+
+def customer_details(request):
+    return HttpResponse("customer details")
