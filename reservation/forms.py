4c06beebf24fe53efc6540c5ebc543e216820502
Chathurika Gajanayaka <chathurikagajanayaka@chathurikas-air.home>
2024-01-13 03:55:13 +0100

    Add reservation page validation

--- reservation/forms.py
+++ reservation/forms.py
@@ -1,6 +1,7 @@
 from django import forms
+from django.utils import timezone
 
-from .models import Reservation, Customer
+from .models import Reservation, Customer, TABLE_CAPACITIES
 
 
 class ReservationModelForm(forms.ModelForm):
@@ -12,12 +13,15 @@ class ReservationModelForm(forms.ModelForm):
         fields = ['reservation_date', 'time_slot', 'number_of_guests', 'table_number']
         exclude = ('customer',)
 
+        max_guests = max([t[0] for t in list(TABLE_CAPACITIES)])
+
         widgets = {
             'reservation_date': forms.DateInput(
                 format=('%Y-%m-%d'),
                 attrs={'class': 'form-control',
                        'placeholder': 'Select a date',
-                       'type': 'date'
+                       'type': 'date',
+                       'min': timezone.now,
                        }),
         }
         labels = {
@@ -26,17 +30,34 @@ class ReservationModelForm(forms.ModelForm):
             'number_of_guests': 'Number Of Guests',
         }
 
-    # def save(self, commit=True):
-    #     """Assign a temporary customer to the reservqtion"""
-    #     obj, created = Customer.objects.get_or_create(
-    #         first_name='Internal',
-    #         last_name='Pending',
-    #         email='temp@thedine',
-    #         temp=True
-    #     )
-    #
-    #     self.cleaned_data['customer'] = obj.id
-    #     return super().save(commit)
+    @staticmethod
+    def send_email():
+        print("Inside send email")
+        pass
+
+    def clean_number_of_guests(self):
+        number_of_guests = self.cleaned_data.get('number_of_guests')
+        max_guests = max([t[0] for t in list(TABLE_CAPACITIES)])
+
+        if number_of_guests is None or number_of_guests == '':
+            raise forms.ValidationError("Number of gusts is required")
+
+        if number_of_guests > max_guests:
+            raise forms.ValidationError("We can support only up to {} guests".format(max_guests))
+
+        if number_of_guests < 1:
+            raise forms.ValidationError("Invalid number of guests")
+
+    def clean_reservation_date(self):
+        reservation_date = self.cleaned_data.get('reservation_date')
+
+        if reservation_date is None or reservation_date == '':
+            raise forms.ValidationError("This field is required")
+
+        if reservation_date < timezone.now().date():
+            raise forms.ValidationError("This field requires today or a future date")
+
+
 
 
 class CustomerModelForm(forms.ModelForm):
